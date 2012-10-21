4719cab5771ee96419c586451aab9420f35d5ffe
thibaultdelor <tibo@invalidcodeexception.com>
2012-10-21 16:06:23 +1100

    - StringBuilder VS StringBuffer refactor

--- src/main/java/com/invalidcodeexception/experiment/langstringbuilder/TestPerfStringBuild.java
+++ src/main/java/com/invalidcodeexception/experiment/langstringbuilder/TestPerfStringBuild.java
@@ -14,103 +14,55 @@ import java.util.concurrent.TimeUnit;
 public class TestPerfStringBuild extends SimpleBenchmark {
 
     private static int NB_THREAD_POOL = 5;
-    private static int NB_THREAD = 100;
 
-    public void timeLogStringBuilder(int reps) {
-        for (int i = 0; i < reps; i++) {
+    private static Runnable WITH_STRING_BUFFER = new Runnable() {
+        @Override
+        public void run() {
             for (int j = 0; j < MyObject.testObjects.length; j++) {
                 MyObject o = MyObject.testObjects[j];
-
-                UnsafeToStringBuilder builder = new UnsafeToStringBuilder(o, new UnsafeToStringStyle() {
-                });
-                builder.append("field1", o.getField1());
-                builder.append("field2", o.getField2());
-                builder.append("field3", o.getField3());
+                new ToStringBuilder(o, ToStringStyle.DEFAULT_STYLE)
+                        .append("field1", o.getField1()).append("field2", o.getField2()).append("field3", o.getField3()).build();
             }
         }
+    };
+    private static Runnable WITH_STRING_BUILDER = new Runnable() {
+        @Override
+        public void run() {
+            for (int j = 0; j < MyObject.testObjects.length; j++) {
+                MyObject o = MyObject.testObjects[j];
+                new UnsafeToStringBuilder(o, new UnsafeToStringStyle(){})
+                        .append("field1", o.getField1()).append("field2", o.getField2()).append("field3", o.getField3()).build();
+            }
+        }
+    };
+
+    private static void sequentialRun(int reps, Runnable r){ for (int i = 0; i < reps; i++) r.run(); }
+    private static void multiThreadRun(int reps, Runnable r) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(NB_THREAD_POOL);
+        for (int i = 0; i < reps; i++) pool.submit(r);
+        pool.shutdown();
+        pool.awaitTermination(20, TimeUnit.SECONDS);
     }
 
     public void timeLogStringBuffer(int reps) {
-        for (int i = 0; i < reps; i++) {
-            for (int j = 0; j < MyObject.testObjects.length; j++) {
-                MyObject o = MyObject.testObjects[j];
-
-                ToStringBuilder builder = new ToStringBuilder(o, ToStringStyle.DEFAULT_STYLE);
-                builder.append("field1", o.getField1());
-                builder.append("field2", o.getField2());
-                builder.append("field3", o.getField3());
-            }
-        }
+        sequentialRun(reps, WITH_STRING_BUFFER);
     }
 
-    public void timeMultiThreadLogStringBuilder(int reps) throws InterruptedException {
-        ExecutorService pool = Executors.newFixedThreadPool(NB_THREAD_POOL);
-        for (int j = 0; j < reps; j++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 0; j < MyObject.testObjects.length; j++) {
-                        MyObject o = MyObject.testObjects[j];
-
-                        UnsafeToStringBuilder builder = new UnsafeToStringBuilder(o, new UnsafeToStringStyle() {
-                        });
-                        builder.append("field1", o.getField1());
-                        builder.append("field2", o.getField2());
-                        builder.append("field3", o.getField3());
-                    }
-                }
-            });
-        }
-        pool.shutdown();
-        pool.awaitTermination(20, TimeUnit.SECONDS);
+    public void timeLogStringBuilder(int reps) {
+        sequentialRun(reps, WITH_STRING_BUILDER);
     }
 
     public void timeMultiThreadLogStringBuffer(int reps) throws InterruptedException {
-        ExecutorService pool = Executors.newFixedThreadPool(NB_THREAD_POOL);
-        for (int j = 0; j < reps; j++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 0; j < MyObject.testObjects.length; j++) {
-                        MyObject o = MyObject.testObjects[j];
-
-                        ToStringBuilder builder = new ToStringBuilder(o, ToStringStyle.DEFAULT_STYLE);
-                        builder.append("field1", o.getField1());
-                        builder.append("field2", o.getField2());
-                        builder.append("field3", o.getField3());
-                    }
-                }
-            });
-        }
-        pool.shutdown();
-        pool.awaitTermination(20, TimeUnit.SECONDS);
+        multiThreadRun(reps, WITH_STRING_BUILDER);
     }
 
-    public void timeMultiThreadNewLogStringBuffer(int reps) throws InterruptedException {
-        ExecutorService pool = Executors.newFixedThreadPool(NB_THREAD_POOL);
-        for (int j = 0; j < reps; j++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 0; j < MyObject.testObjects.length; j++) {
-                        MyObject o = MyObject.testObjects[j];
-
-                        ToStringBuilder builder = new ToStringBuilder(o, new ToStringStyle() {
-                        });
-                        builder.append("field1", o.getField1());
-                        builder.append("field2", o.getField2());
-                        builder.append("field3", o.getField3());
-                    }
-                }
-            });
-        }
-        pool.shutdown();
-        pool.awaitTermination(20, TimeUnit.SECONDS);
+    public void timeMultiThreadLogStringBuilder(int reps) throws InterruptedException {
+        multiThreadRun(reps, WITH_STRING_BUILDER);
     }
 
     public static void main(String[] args) {
         System.out.println(MyObject.testObjects); // init testObjects
-        Runner.main(TestPerfStringBuild.class, new String[]{"--warmupMillis", "5000", "--runMillis", "5000"});
+        Runner.main(TestPerfStringBuild.class, new String[]{"--warmupMillis", "3000", "--runMillis", "5000"});
     }
 
 }
@@ -124,10 +76,7 @@ class MyObject {
     static {
         testObjects = new MyObject[]{
                 new MyObject("field1", 2, "field", "3", "iugacsgiuasgiusaiu", "hcgavusfaucsavusva", "", "wsx"),
-                new MyObject("idgsiugidsdds", Integer.MIN_VALUE, new HashMap<String, String>() {{
-                    put("aa", "bb");
-                    put("dfsfs", "uycdsgfds");
-                }}),
+                new MyObject("idgsiugidsdds", Integer.MIN_VALUE, new HashMap<String, String>() {{ put("aa", "bb"); put("dfsfs", "uycdsgfds");}}),
                 new MyObject("AnOtherExample", Integer.MAX_VALUE),
                 new MyObject("AnOtherExampleLonggg", 0, null, null)
         };
@@ -137,27 +86,9 @@ class MyObject {
         this.field1 = field1;
         this.field2 = field2;
         this.field3 = field3;
-
     }
 
-    public String getField1() {
-        return field1;
-    }
-
-    public int getField2() {
-        return field2;
-    }
-
-    public Object[] getField3() {
-        return field3;
-    }
-
-    @Override
-    public String toString() {
-        ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE);
-        builder.append("field1", field1);
-        builder.append("field2", field2);
-        builder.append("field3", field3);
-        return builder.build();
-    }
+    public String getField1() {return field1;}
+    public int getField2() { return field2; }
+    public Object[] getField3() { return field3; }
 }
